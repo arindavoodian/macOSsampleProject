9608e9abb693b7739f3db963733d4ee9981c0cc9
Arin Davoodian <arindavoodian@apple.com>
2022-10-06 18:02:02 -0700

    Initial Commit

---
+++ macOSsampleProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSsampleProject
+//
+//  Created by Arin Davoodian on 10/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
