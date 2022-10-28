e5cf2a09abb6c7619a5f5c8a3d7962ef665b848d
Arin Davoodian <arindavoodian@apple.com>
2022-10-28 16:40:03 -0700

    added print statement

--- macOSsampleProject/AppDelegate.swift
+++ macOSsampleProject/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        print("applicationWillTerminate")
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
